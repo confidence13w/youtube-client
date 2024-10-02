8fb755d01c59466415a31b3024d0b7b22efbdfd5
Grainer111 <confidence13w@gmail.com>
2024-10-02 13:51:04 +0900

    1002

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import Layout from "./components/Layout";
 import Main from "./pages/Main";
 import Login from "./pages/member/Login";
 import Signup from "./pages/member/Signup";
+import LoginSuccess from "./components/LoginSuccess";
 
 const router = createBrowserRouter([
   {
